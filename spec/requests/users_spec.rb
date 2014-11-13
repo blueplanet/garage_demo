4e94e34e43b4c42780cfaf2092b9ba1eb2691a56
blueplanet <erguolinge@gmail.com>
2014-11-13 16:25:38 +0900

    一覧の詳細の確認を追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,10 +5,19 @@ RSpec.describe "Users", :type => :request do
 
   describe 'GET /api/v1/users' do
     let!(:users) { create_list(:user, 3) }
+    let(:resource_owner) { users.first }
+    let(:users_json) do
+      [
+        { "id" => users[0].id, "name" => users[0].name, "email" => users[0].email },
+        { "id" => users[1].id, "name" => users[1].name, "email" => users[1].email },
+        { "id" => users[2].id, "name" => users[2].name, "email" => users[2].email }
+      ]
+    end
 
     it 'return user resources' do
       get '/api/v1/users', params, env
       expect(response).to have_http_status(200)
+      expect(JSON(response.body)).to match(users_json)
     end
   end
 end
