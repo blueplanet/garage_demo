e3be0a176e2c5929398425a4e623f6cdbeca420a
blueplanet <erguolinge@gmail.com>
2014-11-13 16:08:59 +0900

    Add first spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
+  include RequestHelper
+
+  describe 'GET /api/v1/users' do
+    let!(:users) { create_list(:user, 3) }
+
+    it 'return user resources' do
+      get '/api/v1/users', params, env
       expect(response).to have_http_status(200)
     end
   end
